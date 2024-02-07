e41372352ce8ee18ff8a5dfa875e75ca1fcb33c6
suthinan07 <suthinancheewapanich@gmail.com>
2024-02-07 11:00:20 +0700

    .

---
+++ ProjectVs01.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd 
+
+st.image('')
+st.header("การนำเสนอสถิติการเกิดอุบัติเหตุของประเทศไทย")
+
+col1,col2=st.columns(2)
+
+with col1:
+    st.subheader("จำนวนผู้เสียชีวิต")
+    st.write("2,5600")
+with col2:
+    st.subheader("จำนวนผู้เสียชีวิต")
+    st.write("2,5600)
