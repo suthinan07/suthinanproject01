38c53594088f14da63dc1e336d8724677bfd468f
suthinan07 <suthinancheewapanich@gmail.com>
2024-02-07 12:04:38 +0700

    Update ProjectVs01.py

--- ProjectVs01.py
+++ ProjectVs01.py
@@ -21,6 +21,10 @@ st.write()
 NumM=dt[dt['Sex']=='ชาย'].count()
 NumF=dt[dt['Sex']=='หญิง'].count()
 
-dtSex=[NumM,NumF]
-dtSexb=pd.DataFrame(dtSex)
+st.subheader('ชาย')
+st.subheader(NumM[1])
+st.subheader('หญิง')
+st.subheader(NumF[1])
+dtSex=[NumM[1],NumF[1]]
+dtSexb=pd.DataFrame(dtSex,index=["ชาย","หญิง"])
 st.bar_chart(dtSexb)
